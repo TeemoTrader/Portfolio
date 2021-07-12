db601f44270beb776d608c95d685bdebb3c61192
TeemoTrader <austinnorman1995@gmail.com>
2021-07-12 13:48:49 -0400

    Initial Commit

---
+++ AustinNormanPortfolio/AustinNormanPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AustinNormanPortfolioApp.swift
+//  AustinNormanPortfolio
+//
+//  Created by Teemo Norman on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AustinNormanPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
