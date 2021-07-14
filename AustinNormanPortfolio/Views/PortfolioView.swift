c48bf27eb8a04e3b3baf2b8ec1e33118959a4f3d
TeemoTrader <austinnorman1995@gmail.com>
2021-07-14 14:57:21 -0400

    Added: Secondary portfolio view Assets

--- AustinNormanPortfolio/Views/PortfolioView.swift
+++ AustinNormanPortfolio/Views/PortfolioView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+
 struct PortfolioView: View {
+    
+    var apps = ["Dicey", "CodeHistory", "FlashChat", "FlagGuess", "H4xor"]
+    
+    
     var body: some View {
         NavigationView {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color.white, Color.black]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+
+                ScrollView(.vertical) {
+                    VStack(spacing: 10) {
+                        ForEach(0 ..< apps.count) { app in
+                            Text("\(apps[app]):")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            Image(self.apps[app])
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(Capsule())
+                                .overlay(Capsule().stroke(Color.black, lineWidth: 3))
+                                .shadow(color: .black, radius: 20)
+                                .padding()
+                                
+                                
+                    }
+
+                }
+                    .navigationBarBackButtonHidden(false)
+            
+            
+                
+            }
+                .frame(height: 800)
+            
         }
+
+
+    }
     }
 }
+
+
+
 
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
